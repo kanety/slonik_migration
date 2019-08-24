227b7271daefd2835a6010f65ef2b71090e69198
Yoshikazu Kaneta <kaneta@sitebridge.co.jp>
2019-08-24 15:32:32 +0900

    Test on rails 6.0

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 
 # Pick the frameworks you want:
 require "active_record/railtie"
+require "active_job/railtie"
 
 Bundler.require(*Rails.groups)
 require "slonik_migration"
